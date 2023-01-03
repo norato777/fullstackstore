14273a402f9964a9d562c8c73cd583cc19905c6b
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-03 18:42:26 -0300

    copia de Leo

--- api/src/DB_connect/connect.js
+++ api/src/DB_connect/connect.js
@@ -1,16 +1,19 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
-const password = "holapepe";
-const dbname = "Ecommerce";
-const uri = `mongodb+srv://newStore:${password}@cluster0.phfwwtv.mongodb.net/${dbname}?retryWrites=true&w=majority`;
+const URI = process.env.MONGODB_URI;
+
+// const password = "holapepe";
+// const dbname = "Ecommerce"
+// const uri = `mongodb+srv://newStore:${password}@cluster0.phfwwtv.mongodb.net/${dbname}?retryWrites=true&w=majority`
 
 const connection = () => {
   mongoose.set("strictQuery", true);
-  mongoose.connect(uri);
+  mongoose.connect(URI);
 };
 
 // main().catch(err => console.log(err));
 // async function main() {
-//   await mongoose.connect(`mongodb://leo:${password}@127.0.0.1:27017/test`);
+// await mongoose.connect(`mongodb://leo:${password}@127.0.0.1:27017/test`);
 
 module.exports = connection;
--- api/src/app.js
+++ api/src/app.js
@@ -1,12 +1,11 @@
 const express = require("express");
-const path = require("path");
 
 const app = express();
 
-// ROUTES
+app.use(express.json());
+
+//ROUTES
 const routes = require("./routes/index.routes");
 app.use(routes);
 
-app.listen(3001, () => {
-  console.log("Servidor a la espera de conecciones");
-});
+module.exports = app;
