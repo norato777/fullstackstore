dfef96854ab5e1587d20f1d5f13f874ac32e5c87
NikolasZR <nicolasaberastegui97@gmail.com>
2023-01-04 10:53:21 -0300

    trayendo todo

--- api/src/DB_connect/connect.js
+++ api/src/DB_connect/connect.js
@@ -9,9 +9,7 @@ const connection =()=>{
     mongoose.connect(uri,)
 }
 
-// main().catch(err => console.log(err));
-// async function main() {
-    //   await mongoose.connect(`mongodb://leo:${password}@127.0.0.1:27017/test`);
+
     
     
 
