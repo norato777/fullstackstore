a4a158b9f8d9f330f88429dd134e7a001c5f21a7
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-04 12:25:43 -0300

    Modified

--- api/src/models/products.js
+++ api/src/models/products.js
@@ -2,31 +2,13 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const ProductsSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  image: {
-    type: String,
-  },
-  detail: {
-    type: String,
-  },
-  price: {
-    type: Number,
-  },
-  number: {
-    type: Number,
-  },
-  favorites: {
-    type: Array,
-  },
-  coments: {
-    type: Array,
-  },
-  category: {
-    type: Array,
-  },
+  name: String,
+  image: String,
+  detail: String,
+  price: Number,
+  number: Number,
+  coments: Array,
+  category: Array,
 });
 
 const ProductsModel = mongoose.model("products", ProductsSchema);
