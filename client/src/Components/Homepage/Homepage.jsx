680d8f01b3dfa5835723c78449f2c2bed7a4f8e2
NikolasZR <nicolasaberastegui97@gmail.com>
2023-01-06 14:52:52 -0300

    a

--- client/src/Components/Homepage/Homepage.jsx
+++ client/src/Components/Homepage/Homepage.jsx
@@ -5,11 +5,13 @@ import NavbarMain from "../NavbarMain/NavbarMain";
 import CardBest from "../CardBest/CardBest";
 import AccountCheck from "../AccountCheck/AccountCheck";
 import Carousel from "../Carousel/Carousel";
+import Paginado from "../Paginado/Paginado";
 
 export default function Homepage() {
   return (
     <>
       <Promobar />
+      <Paginado />
       <Header />
       <NavbarMain />
       <CardBest />
---
+++ client/src/Components/Paginado/Paginado.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+
+export default function Paginado ({Products,allProducts , paginado, CurrentPage}){
+    const pageNummbers = []
+
+    for (let i = 1; i <= Math.ceil(allProducts/Products); i++) {
+        pageNummbers.push(i)
+    }
+
+    return (
+        <nav>
+            <ul className="paginado-bar">
+                {pageNummbers && pageNummbers.map ( number =>(
+                    <li className="number" key={number}>
+                        <button onClick={()=> paginado(number)}>{number}</button>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
