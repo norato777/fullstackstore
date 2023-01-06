f40264be06e6a1ee8cd90b9857681036f1850cc8
Mosk09 <leomosconi9@gmail.com>
2023-01-06 13:24:44 -0300

    actualizacion de product controllers y cart

--- api/src/constrollers/index.controllers.js
+++ api/src/constrollers/index.controllers.js
@@ -1,16 +1,15 @@
 const controllers = {};
 // const connection = require("../DB_connect/connect");
 const ProductsModel = require("../models/Product");
-const UserModel = require("../models/Users")
-const CartModel = require("../models/Cart"); 
-
+const UserModel = require("../models/Users");
+const CartModel = require("../models/Cart");
 
 controllers.index = async (req, res) => {
   try {
     const allProducts = await ProductsModel.find();
     const allUsers = await UserModel.find();
     const allCart = await CartModel.find();
-    res.send({ allProducts, allUsers, allCart}); 
+    res.send({ allProducts, allUsers, allCart });
   } catch (error) {
     console.log(error);
   }
--- api/src/routes/cart.js
+++ api/src/routes/cart.js
@@ -1,7 +1,9 @@
-const {Router} = require("express");
-const cartSchema = require("../models/Cart")
-const {postCart,deleteProductCart} = require("../constrollers/cartController")
-
+const { Router } = require("express");
+const cartSchema = require("../models/Cart");
+const {
+  postCart,
+  deleteProductCart,
+} = require("../constrollers/cartController");
 
 const router = Router();
 
@@ -10,49 +12,40 @@ router.get("/", (req, res) => {
   cartSchema
     .find()
     .then((data) => res.json(data))
-    .catch((error) => res.json({ mesagge: error}));
+    .catch((error) => res.json({ mesagge: error }));
 });
 // trae todos los cart (es al pedo pero queria probar)
 router.get("/:id", (req, res) => {
-  const {id} = req.params;
+  const { id } = req.params;
   cartSchema
     .findById(id)
     .then((data) => res.json(data))
-    .catch((error) => res.json({ mesagge: error}));
+    .catch((error) => res.json({ mesagge: error }));
 });
 
-
 router.post("/", async (req, res) => {
-// Obtén los valores de user_id y product_id del cuerpo de la solicitud
-try {
-  const cart = req.body;        
-  console.log(cart)
-  await postCart(cart)
-  console.log(cart)
-  res.json({ message: "Producto agregado al carrito con éxito" })
-  
-} catch (error) {
-     res.send(error.message) 
-    }
-} )
-// Llamar a la función postCart y proporcionar los argumentos user_id y product_id
- 
+  try {
+    // Obtén los valores de user_id y product_id del cuerpo de la solicitud
+    const cart = req.body;
+    // Llamar a la función postCart y proporcionar los argumentos user_id y product_id
+    await postCart(cart);
+    res.json({ message: "Producto agregado al carrito con éxito" });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-
-  router.delete("/:id/:productId", async(req, res) => {
+router.delete("/:id/:productId", async (req, res) => {
+  try {
     // Obtén los valores de user_id y product_id de los parámetros de la ruta
-    try {
-      const cart = req.params
-      // const productId = req.params.productId;
-      
-      // // Llamar a la función deleteProductFromCart y proporcionar los argumentos cart y productId
-      console.log(cart)
-        await deleteProductCart(cart)
-          res.json({ message: "Producto eliminado del carrito con éxito" })
-      
-    } catch (error) {
-      res.send( error.message )
-    }
-    })
+    const cart = req.params;
+    // // Llamar a la función deleteProductCart y proporcionar los argumentos en cart
+    console.log(cart);
+    await deleteProductCart(cart);
+    res.json({ message: "Producto eliminado del carrito con éxito" });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
 
-  module.exports = router
+module.exports = router;
