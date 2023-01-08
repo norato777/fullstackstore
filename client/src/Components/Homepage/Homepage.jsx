850c62e8ac99890eb65829c6227fa4b92b06af30
norato777 <norato777@gmail.com>
2023-01-07 23:59:50 -0600

    styling updated

--- client/src/Components/Homepage/Homepage.jsx
+++ client/src/Components/Homepage/Homepage.jsx
@@ -2,11 +2,7 @@ import React from "react";
 import Promobar from "../Promobar/Promobar";
 import Header from "../Header/Header";
 import NavbarMain from "../NavbarMain/NavbarMain";
-import CardBest from "../CardBest/CardBest";
-import CardAccountCheck from "../CardAccountCheck/CardAccountCheck";
-import Carousel from "../Carousel/Carousel";
-import CardConfigPC from "../CardConfigPC/CardConfigPC";
-import CardJobs from "../CardJobs/CardJobs";
+import PromoArea from "../PromoArea/PromoArea";
 import FeaturedProducts from "../FeaturedProducts/FeaturedProducts";
 import OurBrands from "../OurBrands/OurBrands";
 import FeaturedCategories from "../FeaturedCategories/FeaturedCategories";
@@ -35,11 +31,7 @@ export default function Homepage() {
             <Promobar />
             <Header />
             <NavbarMain />
-            <CardBest />
-            <CardAccountCheck />
-            <Carousel />
-            <CardConfigPC />
-            <CardJobs />
+            <PromoArea />
             <FeaturedProducts />
             <OurBrands />
             <FeaturedCategories />
--- client/src/Components/NavbarMain/NavbarMain.jsx
+++ client/src/Components/NavbarMain/NavbarMain.jsx
@@ -1,48 +1,75 @@
 import React from "react";
 import Nav from "react-bootstrap/Nav";
+import stl from "./NavbarMain.module.css";
 
 export default function NavbarHeader() {
   return (
     <>
       <Nav className="justify-content-center" activeKey="/home">
         <Nav.Item>
-          <Nav.Link href="/">Promociones</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Promociones
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Nuevos productos</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Nuevos productos
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Hardware</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Hardware
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Computadoras</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Computadoras
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Impresion y copiad</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Impresion y copiad
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Audio y video</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Audio y video
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Energia</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Energia
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Punto de venta</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Punto de venta
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Seguridad y vigilancia</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Seguridad y vigilancia
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Telecomunicaciones</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Telecomunicaciones
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Hogar</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Hogar
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Software</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Software
+          </Nav.Link>
         </Nav.Item>
         <Nav.Item>
-          <Nav.Link href="/">Home office</Nav.Link>
+          <Nav.Link className={stl.colors} href="/">
+            Home office
+          </Nav.Link>
         </Nav.Item>
       </Nav>
     </>
