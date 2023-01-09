401d04ff53dc84030917230f6c340543bfe788e2
norato777 <norato777@gmail.com>
2023-01-09 01:27:36 -0600

    updated css

--- client/src/Components/FeaturedProducts/FeaturedProducts.jsx
+++ client/src/Components/FeaturedProducts/FeaturedProducts.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import CardFeaturedProducts from "../CardFeaturedProducts/CardFeaturedProducts";
-import CardFeaturedProducts2 from "../CardFeaturedProducts/CardFeaturedProducts2";
 import stl from "./FeaturedProducts.module.css";
 
 export default function FeaturedProducts() {
@@ -10,11 +9,10 @@ export default function FeaturedProducts() {
       <h1>Productos destacados de hoy</h1>
       <div className={stl.container}>
         <CardFeaturedProducts num={num} />
-        {/* <CardFeaturedProducts num={num + 14} />
+        <CardFeaturedProducts num={num + 14} />
         <CardFeaturedProducts num={num + 35} />
         <CardFeaturedProducts num={num + 63} />
-        <CardFeaturedProducts num={num + 70} /> */}
-        <CardFeaturedProducts2 />
+        <CardFeaturedProducts num={num + 70} />
       </div>
     </>
   );
