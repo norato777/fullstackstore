f7932447fe6b66d9d50803c857f5b9a005386a94
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-14 12:53:59 -0300

    modif

--- client/src/Components/ProductDetail/ProductDetail.jsx
+++ client/src/Components/ProductDetail/ProductDetail.jsx
@@ -9,62 +9,35 @@ import Paypal from "../Paypal/Papypal";
 import Rating_Coments from "../Rating_Coments/Rating_Coments"
 
 const ProductDetail = () => {
-const details = useSelector(state => state.detail)
-
-  const details = useSelector((state) => state.detail);
-  console.log(details);
-
+  const details = useSelector(state => state.detail)
 
   return (
     <>
       <Header />
       <NavbarMain />
-      {details && (
+      {details &&
         <>
-
-            <Header />
-            {details &&
-                <>
-                    <Card className={s.card}>
-                        <Card.Img variant="top" src={details.image} className={s.img} />
-                        <Card.Body>
-                            <Card.Text>
-                                {details.name}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <br />
-                    <Card className={s.cardbody}>
-                        <Card.Body>
-                            <p>{details.description}</p>
-                            <p>{details.price}</p>
-                        </Card.Body>
-                    </Card>
-                    <div className={s.div}>
-                        <Rating_Coments id={(details._id)} />
-                    </div>
-                    <Paypal price={details.price} />
-                </>
-            }
-
           <Card className={s.card}>
             <Card.Img variant="top" src={details.image} className={s.img} />
             <Card.Body>
-              <Card.Text>{details.name}</Card.Text>
+              <Card.Text>
+                {details.name}
+              </Card.Text>
             </Card.Body>
           </Card>
           <br />
           <Card className={s.cardbody}>
             <Card.Body>
-              <Card.Text>
-                <p>{details.description}</p>
-                <p>{details.price}</p>
-              </Card.Text>
+              <p>{details.description}</p>
+              <p>{details.price}</p>
             </Card.Body>
           </Card>
-
+          <div className={s.div}>
+            <Rating_Coments id={(details._id)} />
+          </div>
+          <Paypal price={details.price} />
         </>
-      )}
+      }
       <Footer />
     </>
   );
