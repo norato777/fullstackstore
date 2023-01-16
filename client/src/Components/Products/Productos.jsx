f3f263861d3c9300ba80910555d4db9a614ccd86
Mosk09 <leomosconi9@gmail.com>
2023-01-16 17:06:28 -0300

    aa

--- client/src/Components/Products/Productos.jsx
+++ client/src/Components/Products/Productos.jsx
@@ -3,24 +3,26 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { getDetail, getProducts } from "../../Redux/action";
+import stl from "./Productos.module.css"
 
 export default function Productos() {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const products = useSelector((state) => state.products);
-  // const [products, setProducts] = useState(filtrados);
+  const products = useSelector((state) => state.products); 
   const [page, setPage] = useState(1);
   const [maxPage, setMaxPage] = useState(1);
   const [productsPerPage, setProductsPerPage] = useState(20);
-  const [productsToShow, setProductsToShow] = useState([]);
+
 
   useSelector((state) => state.getDetail);
 
   useEffect(() => {
     setMaxPage(Math.ceil(products.length / productsPerPage));
-    setProductsToShow(
-      products.slice((page - 1) * productsPerPage, page * productsPerPage)
-    );
+    console.log(maxPage)  
+    console.log(products.length) 
+    console.log(productsPerPage) 
+    console.log(Math.ceil(products.length / productsPerPage))
+
   }, [products, page, productsPerPage]);
 
   function handleNextPage() {
@@ -34,49 +36,26 @@ export default function Productos() {
       setPage(page - 1);
     }
   }
-
-  function handleFirstPage() {
-    setPage(1);
-  }
-
-  function handleLastPage() {
-    setPage(maxPage);
-  }
-
-  function handleProductsPerPage(e) {
-    setProductsPerPage(e.target.value);
-  }
-  const handleDetail = (e) => {
+ const handleDetail = (e) => {
     dispatch(getDetail(e.target.value));
     navigate(`/product/${e.target.value}`);
   };
 
   return (
-    <div>
+    <div >
       <div>
-        <select onChange={handleProductsPerPage}>
-          <option value={20}>20</option>
-          <option value={50}>50</option>
-          <option value={100}>100</option>
-        </select>
-      </div>
-      <div>
-        {/* botones de paginacion bootstrap */}
-        <button onClick={handleFirstPage} className="btn btn-primary">
-          Primera
-        </button>
+        {/* botones de paginacion bootstrap */} 
+
         <button onClick={handlePrevPage} className="btn btn-primary">
           Anterior
         </button>
         <button onClick={handleNextPage} className="btn btn-primary">
           Siguiente
-        </button>
-        <button onClick={handleLastPage} className="btn btn-primary">
-          Ultima
-        </button>
+        </button>   
+      <h3>Page {page}</h3>
       </div>
 
-      <div>
+      <div className={stl.container}>
         {/* mensaje de carga mientras se renderiza los productos */}
 
         {products.length === 0 ? (
@@ -88,13 +67,14 @@ export default function Productos() {
           ?.slice((page - 1) * productsPerPage, page * productsPerPage)
           ?.map((product) => {
             return (
-              <div className="card" key={product._id}>
-                <div className="card-body">
-                  <img src={product.image} alt={product.name} />
-                  <h5 className="card-title">{product.name}</h5>
+              <div className={stl.card} key={product._id}>
+                <div className={stl.cardbody}>
+                  <img src={product.image} alt={product.name} className={stl.img}/>
+                 <div className={stl.cardbody1}>
+                  <h5 className="card-title">{product.name.slice(0, 30)}</h5>
                   {/* //descricion con caracteres limitados a 100 */}
                   <p className="card-text">
-                    {product.description.slice(0, 100)}...
+                    {product.description.slice(0, 30)}...
                   </p>
                   <p className="card-text">${product.price}</p>
                   {/* usar getDetail para detalles del producto */}
@@ -105,6 +85,7 @@ export default function Productos() {
                   >
                     Ver detalles
                   </button>
+                  </div>
                 </div>
               </div>
             );
--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -9,7 +9,7 @@ const initialState = {
   orders: [],
   reviews: [],
   category: [],
-  brand:[],
+  brand: [],
   loading: false,
   error: null,
   success: false,
@@ -24,18 +24,20 @@ const initialState = {
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_PRODUCTS":
-      const marcas = state.allProducts.map((e) => e.brand).sort(function (a, b) {
-        if (a < b) return -1;
-        else return 1;
-      });
+      const marcas = state.allProducts
+        .map((e) => e.brand)
+        .sort(function (a, b) {
+          if (a < b) return -1;
+          else return 1;
+        });
       const uniqueBrands = [...new Set(marcas)];
       return {
         ...state,
         products: action.payload,
         allProducts: action.payload,
-        brand:uniqueBrands,
+        brand: uniqueBrands,
       };
-        case "GET_CATEGORIES":
+    case "GET_CATEGORIES":
       return {
         ...state,
         category: action.payload,
@@ -82,11 +84,10 @@ export default function rootReducer(state = initialState, action) {
           products: state.products,
         };
       } else {
-   
         let filtrados = state.products.filter((e) =>
           e.categories.includes(action.payload)
         );
-   
+
         return {
           ...state,
           products: filtrados,
@@ -146,29 +147,33 @@ export default function rootReducer(state = initialState, action) {
         products: state.allProducts,
       };
     case "FILTER_PRICE":
-      let sorted= state.products
+      let sorted1 = state.products;
+      let sorted = sorted1;
       if (action.payload === "Price") {
         return {
           ...state,
-          products: sorted,
+          products: sorted1,
         };
       }
-      if(action.payload === "Menor a mayor") {
-     
-        sorted.sort(function (a,b) { return a.price - b.price})
-        }
-        if(action.payload === "Mayor a menor") {
-          sorted.sort(function (a,b) { return b.price - a.price})
-          }
-        return {
-          ...state,
-          products: sorted,
-        };
-      // }
-      case "PUT_RATING":
-        return {
-          ...state,
-        };
+      if (action.payload === "Menor") {
+        sorted.sort(function (a, b) {
+          return a.price - b.price;
+        });
+      }
+      if (action.payload === "Mayor") {
+        sorted.sort(function (a, b) {
+          return b.price - a.price;
+        });
+      }
+      return {
+        ...state,
+        products: sorted,
+      };
+    // }
+    case "PUT_RATING":
+      return {
+        ...state,
+      };
     default:
       return state;
   }
