f633fa974f7e5c941b58873f354cab6b2a13451f
René Valderrey González <rene_valderrey@hotmail.com>
2023-01-15 21:20:19 -0300

    act

--- client/src/Components/UserProfile/UserProfile.jsx
+++ client/src/Components/UserProfile/UserProfile.jsx
@@ -1,23 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "../Header/Header";
 import NavbarMain from "../NavbarMain/NavbarMain";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import s from './UserProfile.module.css'
+import { useForm } from "react-hook-form";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getUsers } from "../../Redux/action";
+import { getUsers, putUser, signIn, logOut } from "../../Redux/action";
 
 const UserProfile = () => {
     const dispatch = useDispatch()
     const user = useSelector(state => state.user)
+    const users = useSelector(state => state.users)
+    const theUser = users.filter(u => u.email === user.email)
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    console.log(theUser[0]._id)
     console.log(user)
     useEffect(() => {
         dispatch(getUsers())
     }, [])
 
+    const emailChangeHandler = (e) => {
+        setEmail(e.target.value);
+    };
+
+    const passwordChangeHandler = (e) => {
+        setPassword(e.target.value);
+    };
+
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm();
+
+    const onSubmit = () => {
+        const sign = {
+            email: email,
+            password: password,
+        };
+        dispatch(putUser(sign));
+        dispatch(logOut())
+        dispatch(signIn(sign));
+        setEmail("");
+        setPassword("");
+    };
+
     return (
         <>
             <Header />
             <NavbarMain />
-            <h1>User Profile</h1>
+            <div className={s.containerForm}>
+                {user &&
+                    <Form onSubmit={handleSubmit(onSubmit)}>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control type="email"
+                                placeholder={user.email}
+                                {...register("name", { required: true })}
+                                onChange={emailChangeHandler}
+                                value={email} />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control type="password"
+                                placeholder="*****"
+                                {...register("password", { required: true })}
+                                onChange={passwordChangeHandler}
+                                value={password} />
+                        </Form.Group>
+
+                        <Button variant="primary" type="submit">
+                            Modificar
+                        </Button>
+                    </Form>
+                }
+            </div>
         </>
     )
 }
--- client/src/Redux/reducer.js
+++ client/src/Redux/reducer.js
@@ -9,7 +9,7 @@ const initialState = {
   orders: [],
   reviews: [],
   category: [],
-  brand:[],
+  brand: [],
   loading: false,
   error: null,
   success: false,
@@ -24,18 +24,20 @@ const initialState = {
 export default function rootReducer(state = initialState, action) {
   switch (action.type) {
     case "GET_PRODUCTS":
-      const marcas = state.allProducts.map((e) => e.brand).sort(function (a, b) {
-        if (a < b) return -1;
-        else return 1;
-      });
+      const marcas = state.allProducts
+        .map((e) => e.brand)
+        .sort(function (a, b) {
+          if (a < b) return -1;
+          else return 1;
+        });
       const uniqueBrands = [...new Set(marcas)];
       return {
         ...state,
         products: action.payload,
         allProducts: action.payload,
-        brand:uniqueBrands,
+        brand: uniqueBrands,
       };
-        case "GET_CATEGORIES":
+    case "GET_CATEGORIES":
       return {
         ...state,
         category: action.payload,
@@ -74,6 +76,10 @@ export default function rootReducer(state = initialState, action) {
       return {
         ...state,
         products: action.payload,
+      };
+    case "PUT_USER":
+      return {
+        ...state,
       };
     case "FILTER_CATEGORY":
       if (action.payload === "Category") {
@@ -120,17 +126,20 @@ export default function rootReducer(state = initialState, action) {
         return {
           ...state,
           user: [],
+          logged: false,
         };
       } else {
         return {
           ...state,
           user: action.payload,
+          logged: true,
         };
       }
     case "LOG_OUT":
       return {
         ...state,
         user: [],
+        logged: false,
       };
     case "SEARCH_BAR":
       let busqueda = state.allProducts.filter((e) =>
@@ -158,10 +167,10 @@ export default function rootReducer(state = initialState, action) {
           products: filtrados,
         };
       }
-      case "PUT_RATING":
-        return {
-          ...state,
-        };
+    case "PUT_RATING":
+      return {
+        ...state,
+      };
     default:
       return state;
   }
