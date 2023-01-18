553735256ce39b86e85e3db83a9dc65c6cfc8964
Mosk09 <leomosconi9@gmail.com>
2023-01-18 14:47:11 -0300

    ss

--- client/src/Components/CardFeaturedCategories/CardFeaturedCategories.jsx
+++ client/src/Components/CardFeaturedCategories/CardFeaturedCategories.jsx
@@ -1,9 +1,8 @@
 import Card from "react-bootstrap/Card";
 import { useDispatch } from "react-redux";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { filterProductsCategory } from "../../Redux/action";
 import stl from "./CardFeaturedCategories.module.css";
-import Container from "react-bootstrap/esm/Container";
 
 //CardFeaturedCategories es una card que se usa en la pagina principal linki al detalle del producto card title tarjetas de video card img src="./image/tdv(1).webp"
 
@@ -11,7 +10,7 @@ export default function CardFeaturedCategories({ prop }) {
 
   const navigate = useNavigate()
   const dispatch = useDispatch();
-
+  
   const handleClick = (e) => {
     e.preventDefault();
     dispatch(filterProductsCategory(prop.name));
