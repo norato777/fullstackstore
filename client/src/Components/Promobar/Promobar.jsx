96bc95a21c34559890273c60ddc1b2859e6f245f
Edison <norato777@gmail.com>
2023-03-11 12:17:24 -0600

    some updates

--- client/src/Components/Promobar/Promobar.jsx
+++ client/src/Components/Promobar/Promobar.jsx
@@ -4,25 +4,17 @@ import { Container, Card, Figure, Navbar, Row, Image } from "react-bootstrap";
 export default function Promobar() {
   return (
     <>
-      <Container
-        fluid
-        style={{
-          backgroundColor: "#6465667f",
-          backdropFilter: "blur(5px)",
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-          boxShadow:
-            "inset -5px -5px 15px rgba(255,255,255,0.1),inset -5px -5px 15px rgba(0,0,0,0.35)",
-          padding: "7px",
-        }}
-      >
-        <div style={{ marginRight: "7px" }}>
-          <h1>$10Usd </h1>
-        </div>
-        <div style={{ marginBottom: "-15px", color: "var(--brd-yellow)" }}>
-          <h5>de descuento para tu primera compra!</h5>
-        </div>
+      <Container className="mt-3 ">
+        <Figure.Image
+          src="./image/promo1.png"
+          style={{
+            backgroundColor: "rgba(33, 37, 41,0.5)",
+            backdropFilter: "blur(5px)",
+            border: "var(--border)",
+            boxShadow: "var(--box-shadow)",
+          }}
+          className="rounded-4"
+        />
       </Container>
     </>
   );
